72d30ec4cb5d83b10f6fdbc46c832b1910484f39
fajriyan <fajriyan20@gmail.com>
2023-11-01 16:48:49 +0700

    fix error deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import cloudflare from "solid-start-cloudflare-pages";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid({ adapter: cloudflare({}) })],
+});
