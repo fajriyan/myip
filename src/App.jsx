19b565ba8384b07b295feb6edc5e4d9edcb319ae
fajriyan <fajriyan20@gmail.com>
2024-01-05 14:55:30 +0700

    create fallback API

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,14 @@ const App = () => {
 
   return (
     <>
-      {/* bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-amber-200 via-violet-600 to-sky-900 */}
-
-      {/* text-white min-h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-900 via-indigo-950 to-slate-900 */}
       <div className="min-h-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-violet-900 via-indigo-950 to-slate-900">
         <div className="container mx-auto py-7">
           <div className="h-40 bg-white bg-opacity-70 backdrop-filter backdrop-blur-lg flex rounded-md overflow-hidden p-4">
             <div className="w-[60%] ">
               <h1>What is my IP address?</h1>
-              <p className="text-3xl font-bold">{data.ip}</p>
+              <p className="text-3xl font-bold">
+                {data.ip || "API Error | 404"}
+              </p>
               <div className="mt-3">
                 <h2>Lokasi Sekarang :</h2>
                 <p className="-mt-1 font-medium">
@@ -42,19 +41,20 @@ const App = () => {
               <h2 className="font-medium">Network Used</h2>
               <div className="">
                 <div className="flex gap-2">
-                  Registry :<p>{data.network?.registry}</p>
+                  Registry :<p>{data.network?.registry || "undefined"}</p>
                 </div>
                 <div className="flex gap-2">
-                  Organization :<p>{data.network?.organisation}</p>
+                  Organization :
+                  <p>{data.network?.organisation || "undefined"}</p>
                 </div>
                 <div className="flex gap-2">
-                  IP : <p>{data.network?.bgpPrefix}</p> ({" "}
-                  <p>{data.network?.totalAddresses}</p>)
+                  IP : <p>{data.network?.bgpPrefix || "undefined"}</p> ({" "}
+                  <p>{data.network?.totalAddresses || "undefined"}</p>)
                 </div>
 
                 <div className="flex gap-2">
-                  <p>{data.network?.bgpPrefixNetworkAddress}</p> -
-                  <p>{data.network?.bgpPrefixLastAddress}</p>
+                  <p>{data.network?.bgpPrefixNetworkAddress || "undefined"}</p>{" "}
+                  -<p>{data.network?.bgpPrefixLastAddress || "undefined"}</p>
                 </div>
               </div>
             </div>
